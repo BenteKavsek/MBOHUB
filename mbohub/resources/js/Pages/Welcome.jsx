2b26eb1cc23ec04916a291fd7505b6c844ecc259
hasan alumery <hasanalumery@gmail.com>
2025-01-13 22:58:23 +0100

    yt url videos commit

--- mbohub/resources/js/Pages/Welcome.jsx
+++ mbohub/resources/js/Pages/Welcome.jsx
@@ -1,19 +1,17 @@
 import GuestLayout from "@/Layouts/GuestLayout";
+import "./project.css";
 
 function Welcome() {
     return (
         <GuestLayout>
             {/* Heading Section */}
             <section className="heading">
-    <img
-        src="https://mbo-hub.amsterdam/images/mbo-hub-home.png"
-        alt="mbo-hub banner"
-        className="heading__banner"
-    />
-    
-
-</section>
-
+                <img
+                    src="https://mbo-hub.amsterdam/images/mbo-hub-home.png"
+                    alt="mbo-hub banner"
+                    className="heading__banner"
+                />
+            </section>
 
             {/* Events Section */}
             <section className="section events">
@@ -29,10 +27,57 @@ function Welcome() {
 
             {/* Projects Section */}
             <section className="section projects">
-                <h2>Projects</h2>
-                <p>test.</p>
+                <div className="projects-container">
+                    <div className="project-card">
+                        <div className="video-container">
+                            <iframe
+                                src="https://www.youtube.com/embed/dQw4w9WgXcQ"
+                                title="Project Video 1"
+                                frameBorder="0"
+                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                allowFullScreen
+                            ></iframe>
+                        </div>
+                        <p className="project-description">This is a project about creating engaging user interfaces.</p>
+                    </div>
+                    <div className="project-card">
+                        <div className="video-container">
+                            <iframe
+                                src="https://www.youtube.com/embed/3JZ_D3ELwOQ"
+                                title="Project Video 2"
+                                frameBorder="0"
+                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                allowFullScreen
+                            ></iframe>
+                        </div>
+                        <p className="project-description">This project explores the power of API integrations.</p>
+                    </div>
+                    <div className="project-card">
+                        <div className="video-container">
+                            <iframe
+                                src="https://www.youtube.com/embed/V-_O7nl0Ii0"
+                                title="Project Video 3"
+                                frameBorder="0"
+                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                allowFullScreen
+                            ></iframe>
+                        </div>
+                        <p className="project-description">Learn how to build responsive web applications.</p>
+                    </div>
+                    <div className="project-card">
+                        <div className="video-container">
+                            <iframe
+                                src="https://www.youtube.com/embed/2Vv-BfVoq4g"
+                                title="Project Video 4"
+                                frameBorder="0"
+                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                allowFullScreen
+                            ></iframe>
+                        </div>
+                        <p className="project-description">Explore advanced JavaScript techniques and their applications.</p>
+                    </div>
+                </div>
             </section>
-
         </GuestLayout>
     );
 }
